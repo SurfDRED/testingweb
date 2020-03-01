c3eb466392481cfc4c84d2755d185f5b517895b0
Dmitii Iachmentsev <d.yachmentsev@gmail.com>
2020-03-01 17:30:12 +0100

    add tests

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/jpvr9s30voofocip?svg=true)](https://ci.appveyor.com/project/SurfDRED/testingweb)
---
+++ src/test/java/ru/netilogy/testingweb/OrderCardTest.java
@@ -0,0 +1,40 @@
+package ru.netilogy.testingweb;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderCardTest {
+
+    @Test void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Дмитрий Ячменцев");
+        form.$("[data-test-id=phone] input").setValue("+79278913719");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test void shouldSubmitValidname() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Дмитрий Iachmentsev");
+        form.$("[data-test-id=phone] input").setValue("+79278913719");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test void shouldSubmitValidphone() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Дмитрий Ячменцев");
+        form.$("[data-test-id=phone] input").setValue("+79278913719999");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+}
